6e7a7c019ffcc1e08cc7d8ce79d9e78161799a66
baindfrst <103782646+baindfrst@users.noreply.github.com>
2024-09-30 13:21:06 +0300

    lsn

---
+++ program/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+using System.Runtime.InteropServices;
+
+//ОСТАЛОСЬ РЕШИТЬ ПРОБЛЕМУ ТОГО, ЧТО ГЕНОМЫ КОТОРЫЕ СОЗДАЮТСЯ НЕ ОБЯЗАТЕЛЬНО ЯВЛЯЮТСЯ ПОЛНЫМ ЦИКЛОМ
+internal class main
+{
+    static void Main(string[] args)
+    {
+        int sizeMatrix = 25;
+
+        int[][] testmap = GenMatrixCity(sizeMatrix);
+
+        for (int i = 0; i < sizeMatrix; i++)
+        {
+            for (int j = 0; j < sizeMatrix; j++)
+            {
+                Console.Write(testmap[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Population testpopulation = new Population(30, testmap, 10);
+        testpopulation.StartPopulationEvolution();
+    }
+    public static int[][] GenMatrixCity(int size)
+    {
+        Random random = new Random();
+        int[][] matrix = new int[size][];
+        for (int i = 0; i < size; i++)
+        {
+            matrix[i] = new int[size];
+        }
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                int distance = random.Next(1, 100);
+                matrix[i][j] = distance;
+                matrix[j][i] = distance;
+            }
+        }
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                for (int k = j + 1; k < size; k++)
+                {
+                    if (matrix[i][j] + matrix[j][k] < matrix[i][k])
+                    {
+                        matrix[i][k] = matrix[i][j] + matrix[j][k] + 1;
+                        matrix[k][i] = matrix[i][k];
+                    }
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
