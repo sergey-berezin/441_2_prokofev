c9f2bf66d400554d70c14c926b2e1413b159ab4e
baindfrst <103782646+baindfrst@users.noreply.github.com>
2024-09-30 13:25:20 +0300

    lsn commit

---
+++ lib/Genom.cs
@@ -0,0 +1,56 @@
+namespace lib;
+
+public class Genom
+{
+    public int[] cityNumberConections { get; set; }
+    public int GenomScore { get; set; }
+
+    public Genom(int len_genom)
+    {
+        cityNumberConections = new int[len_genom];
+        for (int i = 0; i < len_genom; i++)
+        {
+            cityNumberConections[i] = -1;
+        }
+        cityNumberConections[len_genom - 1] = 0;
+        Random random = new Random();
+        for (int i = 0; i < len_genom - 1; i++)
+        {
+            int next_station = random.Next(1, len_genom);
+            while (Array.IndexOf(cityNumberConections, next_station) != -1)
+            {
+                next_station = random.Next(1, len_genom);
+            }
+            cityNumberConections[i] = next_station;
+        }
+    }
+
+    public int CalculateGenomScore(int[][] CityMap)
+    {
+        GenomScore = 0;
+        for (int i = 0; i < cityNumberConections.Length; i++)
+        {
+            GenomScore += CityMap[i][cityNumberConections[i]];
+        }
+        return GenomScore;
+    }
+
+    public void GenomMutation()
+    {
+        Random random = new Random();
+        int frstPositiom = random.Next(0, cityNumberConections.Length - 1);
+        int scndPosition = random.Next(0, cityNumberConections.Length - 1);
+        while (scndPosition == frstPositiom)
+        {
+            scndPosition = random.Next(0, cityNumberConections.Length - 1);
+        }
+        int tmp = cityNumberConections[frstPositiom];
+        cityNumberConections[frstPositiom] = cityNumberConections[scndPosition];
+        cityNumberConections[scndPosition] = tmp;
+    }
+
+    public override string ToString()
+    {
+        return "0->" + string.Join("->", cityNumberConections);
+    }
+}
